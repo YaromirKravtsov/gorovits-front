ad1a33fc25c582f9937c8384d1f2fcdc1827739e
Yaromir <yaromirkravtosv@gmail.com>
2024-04-11 15:04:17 +0100

    start

--- src/pages/AdminSettingsPage/components/TopMenu/TopMenu.tsx
+++ src/pages/AdminSettingsPage/components/TopMenu/TopMenu.tsx
@@ -6,10 +6,13 @@ import { AdminSettingPageService } from '../../api/AdminSettingPageService'
 import { useActions } from '../../../../hooks/useActions'
 import { getErrorText } from '../../../../helpers/FormDataGeneration'
 import { IUser } from '../../../../models/IUser'
+import RoundIconButton from '../../../../UI/RoundIconButton/RoundIconButton'
+import logoutIcon from '../../../../assets/images/logout-icon.png'
 const TopMenu = () => {
   const [isOpen,setIsOpen] = useState<boolean>(false);
   const [adminInfo,setAdminInfo] = useState<IUser>({} as IUser)
-  const {setGlobalError} = useActions()
+  const {setGlobalError,logout} = useActions()
+
   useEffect(()=>{
     
     const effect = async() =>{
@@ -30,7 +33,7 @@ const TopMenu = () => {
        <MyButton mode='black' onClick={()=> setIsOpen(true)}>
             Passwort ändern 
         </MyButton>
-     
+        <RoundIconButton onClick={() =>logout()} src={logoutIcon} className={style.logOutIcon}/>
         {isOpen&&
           <ChangePasswordMenu isOpenChange={setIsOpen} userId ={adminInfo.id as number}/>
         }
