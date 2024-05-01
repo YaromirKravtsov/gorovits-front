870dc386c85c4888d4e26e71fb75f7fededcc6f3
Yaromir <yaromirkravtosv@gmail.com>
2024-05-01 11:32:05 +0100

    save

--- src/modules/Racket/components/UserRacket/UserRacket.tsx
+++ src/modules/Racket/components/UserRacket/UserRacket.tsx
@@ -25,8 +25,10 @@ const UserRacket: FC<Props> = ({ racket }) => {
 
   useEffect(()=>{
     const fetch = async () =>{
-      setIsRacketAvalivle(await DataActions.checkImageAvailability(racket?.racketModel?.imgLink))
-      setIsStringAvalivle(await DataActions.checkImageAvailability(racket?.pulling.string.imgLink))
+      console.log(racket?.pulling.string.imgLink);
+      console.log(racket?.racketModel?.imgLink)
+      setIsRacketAvalivle(await DataActions.checkImageAvailability(racket?.racketModel?.imgLink) && racket?.racketModel?.imgLink !== null)
+      setIsStringAvalivle(await DataActions.checkImageAvailability(racket?.pulling.string.imgLink) && racket?.racketModel?.imgLink !== null)
     }
     fetch()
   },[])
